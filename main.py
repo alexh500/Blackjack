13065f26a1eb2f81e147e2a78414c092855b8191
Alex Herskovits <alexjoshuah100@gmail.com>
2022-06-19 11:39:16 +0100

    developed the hit and double methods, added some verification in determine_move to make sure that the player is allowed to make that move as it may not be their first move

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Player:
             self.total += i.get_value()
         if self.hand[0].get_name() == "ace" or self.hand[1].get_name() == "ace":
             self.low_total = self.total - 10
-        # self.bet = bet
+        self.bet = 1
 
     def same_name(self):
         return self.hand[0].get_name() == self.hand[1].get_name()
@@ -57,39 +57,45 @@ class Game:
         self.chart = totals
 
     def determine_move(self):
-        print(self.player.hand[0].get_name(), self.player.hand[1].get_name(), self.player.total, self.dealer.up_card.
-              get_name())
-        if self.player.total == 16 and 8 < self.dealer.total or self.player.total == 15 and self.dealer.total == 10:
-            """move = self.surrender()"""
-            print("surrender")
+        finish_move = False
+        first_move = True
+        while not finish_move:
+            print(self.player.hand[0].get_name(), self.player.hand[1].get_name(), self.player.total,
+                  self.dealer.up_card.get_name())
+            if self.player.total == 16 and 8 < self.dealer.total or self.player.total == 15 and self.dealer.total == 10 \
+                    and first_move:
+                """move = self.surrender()"""
+                print("surrender")
 
-        elif self.player.same_name() and self.chart[self.player.hand[0].get_value() + 16][
-            self.dealer.up_card.get_value()
-            - 1] == "y":
-            """move=self.split()"""
-            print("split")
+            elif self.player.same_name() and self.chart[self.player.hand[0].get_value() +
+                                                        16][self.dealer.up_card.get_value() - 1] == "y" and first_move:
+                """move=self.split()"""
+                print("split")
 
-        elif self.player.total < 9 or (self.chart[self.player.total - 7][self.dealer.total - 1] == "h" and
-                                       self.chart[self.player.total - 7][0] == str(self.player.total)):
-            """move = self.hit()"""
-            print("hit")
+            elif self.player.total < 9 or (self.chart[self.player.total - 7][self.dealer.total - 1] == "h" and
+                                           self.chart[self.player.total - 7][0] == str(self.player.total)):
+                move = self.hit()
+                print(move)
 
-        elif self.player.total > 16 or (self.chart[self.player.total - 7][self.dealer.total - 1] == "s" and
-                                        self.chart[self.player.total - 7][0] == str(self.player.total)):
-            """move = self.stand()"""
-            print("stand")
-        else:
-            """move = self.double()"""
-            print("double")
+            elif self.player.total > 16 or (self.chart[self.player.total - 7][self.dealer.total - 1] == "s" and
+                                            self.chart[self.player.total - 7][0] == str(self.player.total)):
+                """move = self.stand()"""
+                print("stand")
+            else:
+                move = self.double()
+                print(move)
+            first_move = False
 
     def hit(self):
-        pass
+        self.player.hand.append(self.deck[random.randint(0, len(self.deck) - 1)])
+        return self.player.hand
 
     def stand(self):
         pass
 
     def double(self):
-        pass
+        self.player.bet *= 2
+        return self.hit(), self.player.bet
 
     def surrender(self):
         pass
