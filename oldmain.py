38f3ff97612d1eb78d2cf4a460c4f503a2dd26bc
aleher0809 <alexjoshuah100@gmail.com>
2022-12-09 16:00:43 +0000

    created shoe class with creating shoe in the init function

--- oldmain.py
+++ oldmain.py
@@ -26,8 +26,35 @@ class Card:
     def get_name(self):
         return self.name
 
-class Round:
+class Shoe:
+    def __init__(self, num_of_decks):
+        self.num_of_decks = num_of_decks
+        names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+        self.deck = []
+        for k in range(0, self.num_of_decks):
+            for i in range(0, 4):
+                for j in names:
+                    self.deck.append(Card(j))
+        random.shuffle(self.deck)
 
+
+
+class Player:
+    def __init__(self):
+
+
+class Dealer:
+    pass
+
+class Round:
+    def __init__(self, game: Game):
+        self.player = Player()
+        self.dealer = Dealer()
+        self.game = game
+
+    def calculate_move(self):
+        while True:
+            if
 
 class Game:
     def __init__(self, rounds_to_play, num_of_decks, deck_penetration):
@@ -36,18 +63,16 @@ class Game:
         self.rounds_to_play = rounds_to_play
         self.rounds_played = 0
         self.rounds_played_list = []
-        self.shoe = self.create_shoe()
+        self.shoe = Shoe(num_of_decks)
         self.num_of_decks = num_of_decks
         self.deck_penetration = deck_penetration
-    def create_shoe(self):
-        shoe = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"] * 4 * self.num_of_decks
-        random.shuffle(shoe)
-        return shoe
+
+
     def simulate(self):
         while self.rounds_played < self.rounds_to_play:
-            self.shoe = self.create_shoe()
+            self.shoe = self.shoe.create_shoe()
             while self.rounds_played < self.rounds_to_play and len(self.shoe) > self.num_of_decks * 52 * self.deck_penetration:
-                round = Round()
+                round = Round(self)
 
 
 
